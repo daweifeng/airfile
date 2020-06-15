54bcba7abd5725b4a8d8ec3cfe4058fa3d66b0f0
Dawei Feng <imdavidf@gmail.com>
2020-06-14 18:18:27 -0700

    Add upload to s3

---
+++ server/src/handler/s3.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+// UploadToS3 will upload file to aws s3
+func UploadToS3(fileName string, fileSize int64, r io.Reader) (string, error) {
+
+	// Read file
+	buffer := make([]byte, fileSize)
+	r.Read(buffer)
+
+	//Create AWS session
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("AWS_REGION")),
+	})
+
+	// Upload to S3
+	_, err = s3.New(sess).PutObject(&s3.PutObjectInput{
+		Bucket:             aws.String(os.Getenv("S3_BUCKET")),
+		Key:                aws.String("files/" + fileName),
+		Body:               bytes.NewReader(buffer),
+		ContentLength:      aws.Int64(fileSize),
+		ContentType:        aws.String(http.DetectContentType(buffer)),
+		ContentDisposition: aws.String("attachment"),
+	})
+
+	return fileName, err
+}
