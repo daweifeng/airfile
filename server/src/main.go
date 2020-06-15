935587f256f0329d8301ff7f62db30bb218a82a3
Dawei Feng <imdavidf@gmail.com>
2020-06-14 18:18:47 -0700

    Update main method

--- server/src/main.go
+++ server/src/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	"dawei.io/app/airfile/controller"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if os.Getenv("ENV") != "prod" {
+		err := godotenv.Load()
+		if err != nil {
+			fmt.Println("Could not load env variables!")
+		}
+	}
+
+	// Logger
+	gin.DisableConsoleColor()
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
 	r := gin.Default()
 
 	// Routes
