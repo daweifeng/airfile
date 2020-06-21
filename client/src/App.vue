bc9e3a422f62221bfc362a4bf1ce7340659d4b5d
Dawei Feng <imdavidf@gmail.com>
2020-06-21 15:57:20 -0700

    Update entries

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      Airfile
       <!-- <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>-->
     </div>
@@ -14,7 +13,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
