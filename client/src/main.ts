8f1567c23fc71d8965f1ec37a51a7f31d34744ac
Dawei Feng <imdavidf@gmail.com>
2020-06-21 15:56:19 -0700

    Add dropzone css

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
 
 Vue.config.productionTip = false
 
