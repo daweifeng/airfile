37eaa9f29554f0c570dff4c230966c8942ccf92b
Dawei Feng <imdavidf@gmail.com>
2020-06-21 17:41:37 -0700

    Use VueClipboard

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+import VueClipboard from 'vue-clipboard2'
+
+Vue.use(VueClipboard)
 
 Vue.config.productionTip = false
 
